4ab84c5ed1b2f5c1462fe1f0193f5ade60d19088
sachin <skodagoda@apm.mc>
2021-08-04 10:38:51 +0800

    springboot app init

---
+++ acmebank/src/main/java/com/bank/acmebank/AcmebankApplication.java
@@ -0,0 +1,13 @@
+package com.bank.acmebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmebankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmebankApplication.class, args);
+	}
+
+}
---
+++ acmebank/src/test/java/com/bank/acmebank/AcmebankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.acmebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmebankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
